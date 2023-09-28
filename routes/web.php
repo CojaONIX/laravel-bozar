800e1e01f32e79ff95a1ddb5245876f44eb18b50
CojaONIX <cojaonix@gmail.com>
2023-09-28 12:51:54 +0200

    3 - Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PostsController;
 
@@ -14,7 +16,19 @@ use App\Http\Controllers\PostsController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
- 
+
 Route::get('/welcome', [PagesController::class, 'showWelcome']);
 Route::get('/', [PagesController::class, 'showHome']);
 Route::get('/post/{id}', [PostsController::class, 'getPostById']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
